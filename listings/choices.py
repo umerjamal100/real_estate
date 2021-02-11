51f52987ff08d7d6f26a964eb195b490fb37eb5e
umerjamal100 <umerjamal109@gmail.com>
2021-02-11 14:04:39 +0500

    inital commit

---
+++ listings/choices.py
@@ -0,0 +1,33 @@
+bedroom_choices={
+'1':1,
+'2':2,
+'3':3,
+'4':4,
+'5':5,
+'6':6,
+}
+
+price_choices={
+'100000':'$100,000',
+'200000':'$200,000',
+'300000':'$300,000',
+'400000':'$400,000',
+'500000':'$500,000',
+'600000':'$700,000',
+'800000':'$800,000',
+'1000000':'1M+'
+}
+
+state_choices={
+'MA':"Massachusetts",
+'AK':'Alaska',
+'AL':'Alabama',
+'AR':'Arkansas',
+'AS':'American Samoa',
+'AZ':'Arizona',
+'CA':'California',
+'GU':"Guam",
+'IA':'Iowa',
+'TX':'Texas',
+'NY':'New York'
+}
